717709cf087df1cb7cb4ad5137b2c960ac371ce5
Bruno Moreira <brunomoreiramg@gmail.com>
2024-06-03 23:48:35 -0300

    chore: Add strings exercises

---
+++ Exercises/strings.js
@@ -0,0 +1,17 @@
+//let name = "Hello World!\n My name is Bruno!";
+let line =  `This is the first line\nThis is the second line`;
+console.log(`Name: ${line}`);
+let con = "Con" + "cat" + "e" + "nate";
+console.log(con);
+let half = `Half of 100 is ${100 / 2}`;
+console.log(half);
+
+console.log(typeof(half));
+
+console.log("Bruno" == "Paloma");
+
+console.log(NaN === NaN);
+
+console.log(true ? 1 : 2);
+
+console.log(false ? 2 : 1);    
