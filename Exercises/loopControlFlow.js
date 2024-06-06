f8ab35380f2e390e85cc466864d51b9d98f5d9d0
Bruno Moreira <brunomoreiramg@gmail.com>
2024-06-05 23:35:56 -0300

    chore: Add loop control flow exercises and what is your name exercise

---
+++ Exercises/loopControlFlow.js
@@ -0,0 +1,30 @@
+let number = 0;
+
+while (number <= 10) {
+    console.log('Using the while: ' + number);
+    number += 2;
+}
+
+for (let number = 0; number <= 10; number = number + 2) {
+    console.log('Using the for loop: ' +number);
+}
+
+let result = 1;
+/*let counter = 0;
+
+while (counter < 10) {
+    result *= 2;
+    counter += 1;
+}
+
+console.log('Using the while: ' + result);
+*/
+for (let counter = 0; counter < 10; counter++) {
+    result *= 2;
+}
+
+console.log('Using the foor loop: ' + result);
+
+//JS value for exponetiation
+
+console.log('Result with exponietation: ' + 2 ** 10);
---
+++ Exercises/whatIsYourName.js
@@ -0,0 +1,8 @@
+let yourName = 'Bruno'
+
+do {
+    console.log('What is your name?');
+    console.log('My name is ' + yourName);
+} while (!yourName);
+
+console.log('Hello ' + yourName);
