312b347b73d602267116e314c6a9607db495d5ef
Bruno Moreira <brunomoreiramg@gmail.com>
2024-06-07 23:33:11 -0300

    Refactor chessBoard.js to print an 8x8 chessboard

--- Exercises/chapter01/chessBoard.js
+++ Exercises/chapter01/chessBoard.js
@@ -4,18 +4,3 @@ for (let i = 0; i < 4; i++) {
     console.log((space + hash) + (space + hash) + (space + hash) + (space + hash));
     console.log((hash + space) + (hash + space) + (hash + space) + (hash + space));
 }
-
-console.log('########################################');
-
-let chessBoard = '';
-for (let x = 0; x < 8; x++) {
-    for (let y = 0; y < 8; y++) {
-        if ((x + y) % 2 == 0) {
-            chessBoard += ' ';
-        } else {
-            chessBoard += '#';
-        }
-    }
-    chessBoard += '\n';
-}
-console.log(chessBoard);
