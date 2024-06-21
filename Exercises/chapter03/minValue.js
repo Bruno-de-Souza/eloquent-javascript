a0bac4ffedf298c62a7ec20946cc54b190aed653
Bruno Moreira <brunomoreiramg@gmail.com>
2024-06-20 22:03:34 -0300

    Minnimun value function

--- Exercises/chapter03/minValue.js
+++ Exercises/chapter03/minValue.js
@@ -1,4 +1,23 @@
 const minValue = function(a, b) {
     return Math.min(a, b);
 }
-console.log(minValue(100, 200));
+console.log(minValue(0, 10)); 
+
+
+const minValueII = (a, b) => {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+console.log(minValueII(0, 10));
+
+const minValueIII = function(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+console.log(minValueIII(3, -2));
