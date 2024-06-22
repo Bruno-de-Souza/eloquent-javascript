cad1318538d23bb5644c6979c05fe9e7e0e87b69
Bruno Moreira <brunomoreiramg@gmail.com>
2024-06-22 00:07:44 -0300

    refactor: Improve isEven function logic and initialization
    The code changes refactor the isEven function in isEven.js to improve its logic and initialization. Instead of using the even variable, the function now directly checks if the number is even. Additionally, the function now handles negative numbers correctly by recursively calling itself with the absolute value. These changes enhance the readability and functionality of the code.

--- Exercises/chapter03/isEven.js
+++ Exercises/chapter03/isEven.js
@@ -1,14 +1,12 @@
-const isEven = (n) => {
-    let even = 0;
+function isEven(n) {
     if (n == 0) {
         return true;
     } else if (n == 1) {
         return false;
-    } else if (isEven(even % 2 == 0)) {
-        n = even;
-        return true;
+    } else if (n < 0) {
+        return isEven(-n);
     } else {
-        return false;
+        return isEven(n - 2);
     }
 }
-console.log(isEven(52));
+console.log(isEven(81));
