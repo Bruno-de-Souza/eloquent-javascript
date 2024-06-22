b28c5c3c6d05c14a1ef1f56d8e51b7d74f54dc40
Bruno Moreira <brunomoreiramg@gmail.com>
2024-06-22 17:08:04 -0300

    Exercise countBs.js

--- Exercises/chapter03/countBs.js
+++ Exercises/chapter03/countBs.js
@@ -1,5 +1,12 @@
 function countBs(n) {
     let str = String(n);
-    return str; 
+    for (let i = 0; i < str.length; i++) {
+        if (str[0] == 'B') {
+            str += i;
+            return str;
+        } else {
+            null;
+        }
+    }
 }
 console.log(countBs());
