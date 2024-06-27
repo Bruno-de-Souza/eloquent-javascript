06ea2d5788b0f7057cbc3f784bb8da6afc14ee22
Bruno Moreira <brunomoreiramg@gmail.com>
2024-06-26 22:45:33 -0300

    Exercise countBs,js function

--- Exercises/chapter03/countBs.js
+++ Exercises/chapter03/countBs.js
@@ -1,11 +1,15 @@
-function countBs(n) {
-    let characters = "BaBaB";
-    for (let i = 0; i < characters.length; i++) {
-        let count = '';
-        if(characters[i] === 'B') {
-            count += i;
-            console.log(count);
+function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == ch) {
+            counted += 1;
         }
     }
+    return counted;
 }
-console.log(countBs('BaBaBa'));
+
+function countBs(string) {
+    return countChar(string, 'B');
+}
+console.log(countChar('The result of the first function is: ' + 'BaBaBaBB', 'a'));
+console.log(countBs(`The Result of the second function: ${'BaBaBaBB'}!`));
