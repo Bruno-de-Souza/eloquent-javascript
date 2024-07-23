9ff72ded477944059ae7e0885d164a1b5e94935a
Bruno Moreira <brunomoreiramg@gmail.com>
2024-07-22 22:48:27 -0300

    refactor: Improve evenOrOdd.js implementation

--- Exercises/Making The Exercises. Again!!!/02/evenOrOdd.js
+++ Exercises/Making The Exercises. Again!!!/02/evenOrOdd.js
@@ -2,11 +2,13 @@ let arr = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
 let evens = [];
 let odds = [];
 
-for (let i = 0; i <= arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 == 0) {
         evens.push(arr[i]);
+        console.log(evens);
     } else {
         odds.push(arr[i]);
+        console.log(odds);
     }
 }
 
