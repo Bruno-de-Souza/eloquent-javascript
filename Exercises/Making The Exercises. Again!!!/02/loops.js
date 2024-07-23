dc90b2263e77acfee60be3c4cc88e4fc7d35cc36
Bruno Moreira <brunomoreiramg@gmail.com>
2024-07-22 21:54:19 -0300

    Some JS exercises

---
+++ Exercises/Making The Exercises. Again!!!/02/loops.js
@@ -0,0 +1,23 @@
+//Practice makes better!
+
+for (let j = 1; j <= 5; j++) {
+    console.log('Print the number', j);
+}
+
+console.log('===================================');
+
+for (let h = 3; h <=29; h++) {
+    //if (h % 2 ==! 0) console.log('The odd value is:', h);
+}
+
+console.log('===================================');
+
+for (let l = 12; l >= -14; --l) {
+    if (l % 2 == 0) console.log('The even value is:', l);
+}
+
+console.log('===================================');
+
+for (let k = 50; k >= 20; k--) {
+    if (k % 3 == 0) console.log('Numbers multiplied by 3 is:', k); 
+}
---
+++ Exercises/Making The Exercises. Again!!!/02/loopsII.js
@@ -0,0 +1,10 @@
+let lc = 'launchCode';
+let arr = [1, 5, 'LC101', 'blue', 42];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log('Meu array is: ' + arr[i]);
+}
+
+for (let j = lc.length - 1; j >= 0; j--) { 
+    console.log(lc[j]);
+}
