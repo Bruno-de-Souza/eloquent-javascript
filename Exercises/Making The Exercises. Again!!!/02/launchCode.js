bf88ab64f5b8e53ce011cafc1227780b0ae4599b
Bruno Moreira <brunomoreiramg@gmail.com>
2024-07-23 21:57:29 -0300

    launchCode.js exercise

---
+++ Exercises/Making The Exercises. Again!!!/02/launchCode.js
@@ -0,0 +1,7 @@
+const input = require('readline-sync');
+
+let iniCombustivel = 0, numAstronautas = 0, altitude = 0;
+
+while (iniCombustivel > 5000 && iniCombustivel < 30000) {
+    let info = input.question("Informe o número de astronautas: ");   
+}
--- Exercises/Making The Exercises. Again!!!/02/whileLoop.js
+++
@@ -1,7 +0,0 @@
-const input = require('readline-sync');
-
-let nivelCombustivel = 0;
-let numAstronautas = 0;
-let altitude = 0;
-
-let info = input.question('Hello World? ');
