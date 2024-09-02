ba3e03fdd62a23cf0c832d16a614ecf38b89223c
Bruno Moreira <brunomoreiramg@gmail.com>
2024-09-01 21:44:15 -0300

    chore: Refactor sumOfNaturalNumbers function to handle invalid input

--- Exercises/Making The Exercises. Again!!!/03/sumOfNaturalNumbers.js
+++ Exercises/Making The Exercises. Again!!!/03/sumOfNaturalNumbers.js
@@ -1,11 +1,17 @@
 /*
     Write a recursive function that returns the sum of the first "n" natural numbers
+    sumOfNaturalNumbers(5) = should return 15 because(1 + 2 + 3 + 4 + 5) 
 */
 function sumOfNaturalNumbers(n) {
     if (n == 1) {
         return 1;
-    } else {
-        return sumOfNaturalNumbers(n);
+    } else if (n < 0 || n == 0) {
+        return 'Value Invalid!';
+    }
+    else {
+        return n + sumOfNaturalNumbers( n - 1);
     }
 }
-console.log(sumOfNaturalNumbers(2));
+console.log(sumOfNaturalNumbers(5));
+
+// Testing: 5 + 4 + 3 + 2 + 1 = 15
